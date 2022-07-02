8fe0dca7e356d8d410dc198d35020d3482e1d531
epicfacepace <braydenioneal@gmail.com>
2022-07-02 12:22:39 -0500

    Fixed cable model misalignment

--- src/main/java/com/braydenoneal/blogic/BlogicModClient.java
+++ src/main/java/com/braydenoneal/blogic/BlogicModClient.java
@@ -1,8 +1,13 @@
 package com.braydenoneal.blogic;
 
+import com.braydenoneal.blogic.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class BlogicModClient implements ClientModInitializer {
 	@Override
-	public void onInitializeClient() {}
+	public void onInitializeClient() {
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.CABLE, RenderLayer.getCutout());
+	}
 }
