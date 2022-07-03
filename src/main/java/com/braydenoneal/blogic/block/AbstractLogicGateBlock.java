d957b6b150b132c88acd08ac229844cf918ce9e2
epicfacepace <braydenioneal@gmail.com>
2022-07-03 10:44:43 -0500

    Removed unnecessary properties from logic gate blocks

--- src/main/java/com/braydenoneal/blogic/block/AbstractLogicGateBlock.java
+++ src/main/java/com/braydenoneal/blogic/block/AbstractLogicGateBlock.java
@@ -7,8 +7,6 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -18,22 +16,17 @@ import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class AbstractLogicGateBlock extends FacingBlock {
-	private static final IntProperty POWERED_INPUTS = IntProperty.of("powered_inputs", 0, 4);
-	private static final BooleanProperty INVERTED = BooleanProperty.of("inverted");
-
 	protected AbstractLogicGateBlock(Settings settings) {
 		super(settings);
 		this.setDefaultState(this.getStateManager().getDefaultState()
 				.with(Properties.FACING, Direction.NORTH)
 				.with(Properties.POWERED, false)
-				.with(POWERED_INPUTS, 0)
-				.with(INVERTED, false)
 		);
 	}
 
 	@Override
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-		builder.add(Properties.FACING, Properties.POWERED, POWERED_INPUTS, INVERTED);
+		builder.add(Properties.FACING, Properties.POWERED);
 	}
 
 	@Nullable
@@ -41,26 +34,20 @@ public abstract class AbstractLogicGateBlock extends FacingBlock {
 	public BlockState getPlacementState(ItemPlacementContext ctx) {
 		Direction facing = ctx.getPlayerLookDirection();
 		return this.getDefaultState().with(Properties.FACING, facing)
-				.with(Properties.POWERED, shouldBePowered(ctx.getWorld(), ctx.getBlockPos(), facing))
-				.with(POWERED_INPUTS, getPoweredInputs(ctx.getWorld(), ctx.getBlockPos(), facing))
-				.with(INVERTED, getInverted(ctx.getWorld(), ctx.getBlockPos(), facing));
+				.with(Properties.POWERED, shouldBePowered(ctx.getWorld(), ctx.getBlockPos(), facing));
 	}
 
 	@Override
 	public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
 		world.setBlockState(pos, state
-				.with(Properties.POWERED, shouldBePowered(world, pos, getFacing(state)))
-				.with(POWERED_INPUTS, getPoweredInputs(world, pos, getFacing(state)))
-				.with(INVERTED, getInverted(world, pos, getFacing(state))));
+				.with(Properties.POWERED, shouldBePowered(world, pos, getFacing(state))));
 		super.onPlaced(world, pos, state, placer, itemStack);
 	}
 
 	@Override
 	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
 		world.updateNeighbors(pos, state.getBlock());
-		return state.with(Properties.POWERED, shouldBePowered(world, pos, getFacing(state)))
-				.with(POWERED_INPUTS, getPoweredInputs(world, pos, getFacing(state)))
-				.with(INVERTED, getInverted(world, pos, getFacing(state)));
+		return state.with(Properties.POWERED, shouldBePowered(world, pos, getFacing(state)));
 	}
 
 	@Override
