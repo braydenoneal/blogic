e6819dda851cfddff58c3afd49296f69c4922a68
Brayden O'Neal <braydenoneal@protonmail.com>
2025-07-16 09:55:59 -0500

    Remove unused imports

--- src/main/java/com/braydenoneal/block/CableBlock.java
+++ src/main/java/com/braydenoneal/block/CableBlock.java
@@ -1,20 +1,12 @@
 package com.braydenoneal.block;
 
 import com.braydenoneal.block.entity.AbstractNetworkBlockEntity;
-import com.braydenoneal.block.entity.RedstoneReaderBlockEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.block.WireOrientation;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
 
 public class CableBlock extends Block {
     public CableBlock(Settings settings) {
