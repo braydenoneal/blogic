93c66f24134fd33c44cd3b414434689e56f8685e
Brayden O'Neal <braydenoneal@protonmail.com>
2025-07-24 14:45:20 -0500

    Call function work in progress

--- src/main/java/com/braydenoneal/data/controller/function2/Parameters.java
+++ src/main/java/com/braydenoneal/data/controller/function2/Parameters.java
@@ -7,6 +7,8 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.SimpleRegistry;
 import net.minecraft.util.Identifier;
 
+import java.util.List;
+
 public class Parameters {
     public static final Registry<Class<?>> REGISTRY = new SimpleRegistry<>(
             RegistryKey.ofRegistry(Identifier.of("blogic", "parameter_types")), Lifecycle.stable()
@@ -16,6 +18,7 @@ public class Parameters {
 
     public static final Class<?> BOOLEAN = register("boolean", Boolean.class);
     public static final Class<?> INTEGER = register("integer", Integer.class);
+    public static final Class<?> LIST = register("list", List.class);
 
     public static Class<?> register(String id, Class<?> type) {
         return Registry.register(REGISTRY, Identifier.of("blogic", id), type);
